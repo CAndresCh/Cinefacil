8cf399de98cbeac8a402edda5504b170a31eebc8
34carlos.ch <34carlosa.ch@gmail.com>
2025-07-29 21:47:46 -0600

    Se agrego la funcion de cancelar la reservacion

--- cinefacil.py
+++ cinefacil.py
@@ -59,13 +59,28 @@ def mostrar_reservas():
         print(f"Película: {r['pelicula']} ({r['hora']})")
         print(f"Boletos: {r['boletos']} - Total: Q{r['total']:.2f}")
 
+def cancelar_reserva():
+    nombre = input("Ingrese el nombre del cliente para cancelar la reserva: ").strip()
+
+    nuevas_reservas = [r for r in reservas if r["cliente"].lower() != nombre.lower()]
+
+    if len(nuevas_reservas) < len(reservas):
+        for i in range(len(reservas) - 1, -1, -1):
+            if reservas[i]["cliente"].lower() == nombre.lower():
+                del reservas[i]
+        print("Reserva cancelada exitosamente.")
+    else:
+        print("No se encontró una reserva con ese nombre.")
+
+
 def menu():
     while True:
         print("\nSistema CineFácil")
         print("1. Hacer una reserva")
         print("2. Mostrar funciones disponibles")
         print("3. Mostrar todas las reservas")
-        print("4. Salir")
+        print("4. Cancelar una reserva")
+        print("5. Salir")
         opcion = input("Selecciona una opción: ")
 
         if opcion == "1":
@@ -75,6 +90,8 @@ def menu():
         elif opcion == "3":
             mostrar_reservas()
         elif opcion == "4":
+            cancelar_reserva()
+        elif opcion == "5":
             print("Gracias por usar CineFácil.")
             break
         else:
